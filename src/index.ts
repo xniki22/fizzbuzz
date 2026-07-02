8513851420943cba3e8deb1e17a0c2d54a7c4b6a
Nikitha Cano <canonikitha@gmail.com>
2026-07-02 07:32:42 -0700

    added solution

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Happy developing ✨')
+function getLabel(n: number): string {
+    const divisibleBy3 = n % 3 === 0;
+    const divisibleBy5 = n % 5 === 0;
+
+    if (divisibleBy3 && divisibleBy5) return "fizzbuzz";
+    if (divisibleBy3) return "fizz";
+    if (divisibleBy5) return "buzz";
+    return "";
+}
+
+for (let i = 1; i <= 100; i++) {
+    const label = getLabel(i);
+    console.log(label ? `${i} ${label}` : `${i}`);
+}
